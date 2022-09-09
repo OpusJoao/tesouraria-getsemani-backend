bd2e3682fcab23dea4bccd2a229708fca1a444df
João Ferreira <joao.ferreira@gruponewway.com.br>
2022-09-09 00:28:30 -0300

    Revert "feat"
    This reverts commit 2d443a1ff5b16877f015589c23086f4139d397f8.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import express, { Request, Response } from 'express'
-import router from './routes'
 const app = express()
 
-app.use(router)
 
 app.get('/healthcheck', (req: Request, res: Response) => {
   res.send({message: 'ok'})
--- src/routes.ts
+++
@@ -1,8 +0,0 @@
-import { request, Request, Response, Router } from "express";
-import PaymentsController from "./controllers/paymentsController";
-
-const router = Router()
-
-router.get('/payments/', PaymentsController.getAll)
-
-export default router
