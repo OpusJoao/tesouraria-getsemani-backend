d8ac92a547ab99224c3c07fd12728ceebacb9dc2
João Ferreira <joao.ferreira@gruponewway.com.br>
2022-09-09 10:17:33 -0300

    refactor: change name of folders to singular

--- src/controllers/paymentsController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, response, Response } from "express";
-
-export default class PaymentsController{
-  static getAll(req: Request, res: Response): Response{
-    return res.send({
-      message: 'ok'
-    })
-  }
-}
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import HealthCheckController from "./controllers/healthcheckController";
-import PaymentsController from "./controllers/paymentsController";
-
+import HealthCheckController from "./controller/healthcheckController";
+import PaymentsController from "./controller/paymentsController";
+import PaymentsService from "./service/paymentsService";
+import PaymentsRepository from './database/payments'
+const paymentsController = new PaymentsController(new PaymentsService(PaymentsRepository))
 const router = Router()
 
 router.get('/payments/', PaymentsController.getAll)
