087843edb9daebeacd2b0266eea47ced2a391aa7
João Ferreira <joao.ferreira@gruponewway.com.br>
2022-09-09 11:31:14 -0300

    refactor: ensure healthCheck controller use adapter from express

--- src/routes.ts
+++ src/routes.ts
@@ -2,9 +2,10 @@ import { Router } from "express";
 import HealthCheckController from "./controller/healthcheckController";
 import { adaptRoute } from "./ adapter/expressRouterAdapter";
 import { makeGetAllPaymentsController } from "./factory/makeGetAllPaymentsController";
+import { makeHealthCheckController } from "./factory/makeHealthCheckController";
 const router = Router()
 
 router.get('/payments/', adaptRoute(makeGetAllPaymentsController()))
-router.get('/healthcheck/', HealthCheckController.index)
+router.get('/healthcheck/', adaptRoute(makeHealthCheckController()))
 
 export default router
