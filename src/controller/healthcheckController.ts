a21e0083ecc24a9d969a3e6eae739d8fa96a070e
João Ferreira <joao.ferreira@gruponewway.com.br>
2022-09-09 11:30:28 -0300

    refactor: ensure healthCheck implements controller pattern

--- src/controller/healthcheckController.ts
+++ src/controller/healthcheckController.ts
@@ -1,9 +1,14 @@
-import { Request, Response } from "express";
+import ControllerInterface from "../interface/IController";
+import HttpRequest from "../interface/IHttpRequest";
+import HttpResponse from "../interface/IHttpResponse";
 
-export default class HealthCheckController{
-  static index(req: Request, res: Response): Response{
-    return res.send({
-      message: 'healthy'
-    })
+export default class HealthCheckController implements ControllerInterface{
+  handle(req: HttpRequest): HttpResponse{
+    return {
+      statusCode: 200,
+      body: {
+        message: 'healthy'
+      }
+    }
   }
 }
