1f26510007b10da7dc2c5fa039cc737229a1547e
João Ferreira <joao.ferreira@gruponewway.com.br>
2022-09-09 13:03:45 -0300

    feat: add create payment route

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { adaptRoute } from "./ adapter/expressRouterAdapter";
+import { makeCreatePaymentController } from "./factory/makeCreatePaymentsController";
 import { makeGetAllPaymentsController } from "./factory/makeGetAllPaymentsController";
 import { makeHealthCheckController } from "./factory/makeHealthCheckController";
 const router = Router()
 
 router.get('/payments/', adaptRoute(makeGetAllPaymentsController()))
+router.post('/payments/', adaptRoute(makeCreatePaymentController()))
 router.get('/healthcheck/', adaptRoute(makeHealthCheckController()))
 
 export default router
