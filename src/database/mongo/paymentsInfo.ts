5738aec29a124327e0cd069f236c95b7ea2231b8
João Ferreira <joao.ferreira@gruponewway.com.br>
2022-09-09 16:43:18 -0300

    feat: add paymentInfo model

---
+++ src/database/mongo/paymentsInfo.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from 'mongoose';
+
+const PaymentsInfoSchema = new Schema({
+  title: String,
+  icon: String,
+  treasurer: String,
+  formatedPhone: String,
+  phone: String,
+  pix: {
+    name: String,
+    value: Number,
+    bank: String,
+    code: String
+  },
+  ted: {
+    name: String,
+    document: String,
+    accountNumber: Number,
+    agency: Number,
+    typeAccount: String
+  }
+},{
+  timestamps: true
+})
+
+export default mongoose.model('paymentsInfo', PaymentsInfoSchema)
+
