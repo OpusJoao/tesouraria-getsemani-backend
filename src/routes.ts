5b91e32008c4f9157533bf6b8e8a103cbdefa855
João Ferreira <joao.ferreira@gruponewway.com.br>
2022-09-09 16:58:37 -0300

    feat: add paymentinfo route

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { adaptRoute } from "./ adapter/expressRouterAdapter";
 import { makeCreatePaymentController } from "./factory/makeCreatePaymentsController";
 import { makeGetAllPaymentsController } from "./factory/makeGetAllPaymentsController";
+import { makeGetPaymentInfoByPaymentId } from "./factory/makeGetPaymentInfoByPaymentId";
 import { makeHealthCheckController } from "./factory/makeHealthCheckController";
 const router = Router()
 
 router.get('/payments/', adaptRoute(makeGetAllPaymentsController()))
 router.post('/payments/', adaptRoute(makeCreatePaymentController()))
+router.get('/paymentsInfo/:paymentId', adaptRoute(makeGetPaymentInfoByPaymentId()))
 router.get('/healthcheck/', adaptRoute(makeHealthCheckController()))
 
 export default router
