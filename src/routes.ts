9c4b8ad89957f2ce8cd0160965ecbc93dc97027a
João Ferreira <joao.ferreira@gruponewway.com.br>
2022-09-09 11:02:32 -0300

    refactor: ensure router use express adpater

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import HealthCheckController from "./controller/healthcheckController";
-import PaymentsController from "./controller/paymentsController";
-import PaymentsService from "./service/paymentsService";
-import PaymentsRepository from './database/payments'
-const paymentsController = new PaymentsController(new PaymentsService(PaymentsRepository))
+import { adaptRoute } from "./ adapter/expressRouterAdapter";
+import { makeGetAllPaymentsController } from "./factory/makeGetAllPaymentsController";
 const router = Router()
 
-router.get('/payments/', PaymentsController.getAll)
+router.get('/payments/', adaptRoute(makeGetAllPaymentsController()))
 router.get('/healthcheck/', HealthCheckController.index)
 
 export default router
