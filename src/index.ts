2d443a1ff5b16877f015589c23086f4139d397f8
João Ferreira <joao.ferreira@gruponewway.com.br>
2022-09-09 00:27:55 -0300

    feat

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express'
+import router from './routes'
 const app = express()
 
+app.use(router)
 
 app.get('/healthcheck', (req: Request, res: Response) => {
   res.send({message: 'ok'})
