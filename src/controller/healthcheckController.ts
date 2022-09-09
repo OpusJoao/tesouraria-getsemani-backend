274f32a35bfa810e68237683ff4845a4209e6f19
João Ferreira <joao.ferreira@gruponewway.com.br>
2022-09-09 16:42:26 -0300

    refactor: add prop name on healthcheck controller class

--- src/controller/healthcheckController.ts
+++ src/controller/healthcheckController.ts
@@ -3,6 +3,7 @@ import HttpRequest from "../interface/IHttpRequest";
 import HttpResponse from "../interface/IHttpResponse";
 
 export default class HealthCheckController implements ControllerInterface{
+  public readonly name: string = 'HealthCheckController'
   handle(req: HttpRequest): HttpResponse{
     return {
       statusCode: 200,
