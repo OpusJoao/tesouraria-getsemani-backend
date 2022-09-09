1d0c4650368cde537471bd66d35d4ed94f96b33b
João Ferreira <joao.ferreira@gruponewway.com.br>
2022-09-09 00:57:52 -0300

    feat: add connection with database

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import router from './routes'
 import mongoose from 'mongoose'
 const app = express()
 
+mongoose.connect('mongodb+srv://admin:F0JfqC4GrciQkdoK@treasury.lffsy4b.mongodb.net/?retryWrites=true&w=majority').then(()=>{
+  console.log(`[Server] Database connected successfully`)
+}).catch(e => {
+  console.error(`[Server] Error while trying to connect database: `, {message: e?.message, stack: e?.stack})
+})
 
 app.use(router)
 app.listen(3000, ()=>{
