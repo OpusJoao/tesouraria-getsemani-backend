5c1cdc8e2e4522452ac0a25f2d5f962f928f9d14
João Ferreira <joao.ferreira@gruponewway.com.br>
2022-09-09 00:39:09 -0300

    chore: add mongoose and types/mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 import router from './routes'
+import mongoose from 'mongoose'
 const app = express()
 
-app.use(router)
 
+app.use(router)
 app.listen(3000, ()=>{
   console.log(`Server is running on port 3000`)
 })
