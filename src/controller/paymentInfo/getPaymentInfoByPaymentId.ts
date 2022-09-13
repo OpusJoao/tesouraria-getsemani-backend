92f51e8b82947430f5904e2a7d8bf48c235d601e
João Ferreira <joao.ferreira@gruponewway.com.br>
2022-09-13 18:25:57 -0300

    feat: ensure getpaymentinfobypaymentid controller get data from mongoose repository

--- src/controller/paymentInfo/getPaymentInfoByPaymentId.ts
+++ src/controller/paymentInfo/getPaymentInfoByPaymentId.ts
@@ -6,9 +6,18 @@ import PaymentsService from "../../service/paymentsService";
 
 export default class GetPaymentInfoByPaymentId implements ControllerInterface{
   public readonly name: String = 'GetPaymentInfoByPaymentId';
-  constructor(private readonly paymentInfoService: PaymentsInfoService, payment: PaymentsService){}
+  constructor(private readonly paymentInfoService: PaymentsInfoService, private readonly paymentService: PaymentsService){}
   handle(req: IHttpRequest): IHttpResponse | Promise<IHttpResponse> {
-    return { statusCode: 200, body:{message: 'test ok'}}
+    const { paymentId } = req.params
+
+    try{
+      const isValidPayment = this.paymentService.isValid(paymentId)
+      return { statusCode: 200, body:{message: 'test ok', payment: isValidPayment}}
+    }catch(e){
+      console.log(e)
+      return { statusCode: 200, body:{message: 'test ok'}}
+    }
+    
   }
   
 }
