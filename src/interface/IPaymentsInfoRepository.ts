a837473d64e6331937744483deac657f1ed6c34f
OpusJoao <projetos.joao236@gmail.com>
2022-09-15 01:29:21 -0300

    feat: create method find

--- src/interface/IPaymentsInfoRepository.ts
+++ src/interface/IPaymentsInfoRepository.ts
@@ -4,7 +4,8 @@ import IPaymentsInfo from "./IPaymentsInfo";
 export default interface IPaymentsInfoRepository{
   create(payment: IPayments): Promise<IPaymentsInfo>,
   findAll(): Promise<IPaymentsInfo[]>,
-  findAById(idPayment: String): Promise<IPaymentsInfo>,
+  findById(idPayment: String): Promise<IPaymentsInfo>,
+  find(query: object): Promise<IPaymentsInfo[]>
   update(): Promise<IPaymentsInfo>,
   delete(): Promise<Boolean>,
 }
