31d14df12619853f8dce21f949756ffabd89dc59
OpusJoao <projetos.joao236@gmail.com>
2022-09-15 01:31:00 -0300

    feat: implements new methods on mongoPaymentsInfoRepository

--- src/repository/mongoPaymentInfoRepository.ts
+++ src/repository/mongoPaymentInfoRepository.ts
@@ -1,8 +1,16 @@
-import PaymentsModel from '../database/mongo/payments'
+import PaymentsInfoModel from '../database/mongo/paymentsInfo'
 import IPaymentsInfo from "../interface/IPaymentsInfo";
 import IPaymentsInfoRepository from "../interface/IPaymentsInfoRepository";
 
 export default class MongoPaymentsInfoRepository implements IPaymentsInfoRepository {
+  findById(idPayment: String): Promise<IPaymentsInfo> {
+    throw new Error('Method not implemented.');
+  }
+  find(query: object): Promise<IPaymentsInfo[]> {
+    return new Promise((resolve, reject) => {
+      resolve(PaymentsInfoModel.find(query))
+    })
+  }
   findAll(): Promise<IPaymentsInfo[]> {
     throw new Error('Method not implemented.');
   }
@@ -15,8 +23,40 @@ export default class MongoPaymentsInfoRepository implements IPaymentsInfoReposit
   delete(): Promise<Boolean> {
     throw new Error('Method not implemented.');
   }
-  create(payment: IPaymentsInfo): Promise<IPaymentsInfo> {
-    throw new Error('Method not implemented.');
+  async create(paymentInfo: IPaymentsInfo): Promise<IPaymentsInfo> {
+    return new Promise( async (resolve, reject) => {
+      try{
+        const createdPaymentInfo = await PaymentsInfoModel.create(paymentInfo)
+        if(createdPaymentInfo){
+          resolve({
+            paymentId: createdPaymentInfo?.paymentId || '',
+            title: createdPaymentInfo?.title || '',
+            icon: createdPaymentInfo?.icon || '',
+            treasurer: createdPaymentInfo?.treasurer || '',
+            formatedPhone: createdPaymentInfo?.formatedPhone || '',
+            phone: createdPaymentInfo?.phone || '',
+            pix: {
+              name: createdPaymentInfo?.phone || '',
+              value: createdPaymentInfo?.value || '',
+              bank: createdPaymentInfo?.bank || '',
+              code: createdPaymentInfo?.code || '',
+            },
+            ted: {
+              name: createdPaymentInfo?.name || '',
+              document: createdPaymentInfo?.document || '',
+              accountNumber: createdPaymentInfo?.accountNumber || '',
+              agency: createdPaymentInfo?.agency || '',
+              typeAccount: createdPaymentInfo?.typeAccount || '',
+            }
+          })
+        }else{
+          reject('Error while trying to create PaymentInfo')
+        }
+      }catch(e){
+        reject(e)
+      }
+
+    })
   }
 
 }
