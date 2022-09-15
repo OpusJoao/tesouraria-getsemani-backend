5752973b39f8a7caa83fbfde485ecbc13da7e2a2
OpusJoao <projetos.joao236@gmail.com>
2022-09-15 01:33:59 -0300

    feat: ensure getPaymentInfo controller get data from database

--- src/controller/paymentInfo/getPaymentInfoByPaymentId.ts
+++ src/controller/paymentInfo/getPaymentInfoByPaymentId.ts
@@ -7,15 +7,15 @@ import PaymentsService from "../../service/paymentsService";
 export default class GetPaymentInfoByPaymentId implements ControllerInterface{
   public readonly name: String = 'GetPaymentInfoByPaymentId';
   constructor(private readonly paymentInfoService: PaymentsInfoService, private readonly paymentService: PaymentsService){}
-  handle(req: IHttpRequest): IHttpResponse | Promise<IHttpResponse> {
+  async handle(req: IHttpRequest): Promise<IHttpResponse> {
     const { paymentId } = req.params
 
     try{
-      const isValidPayment = this.paymentService.isValid(paymentId)
+      const isValidPayment = await this.paymentInfoService.findByPaymentId(paymentId)
       return { statusCode: 200, body:{message: 'test ok', payment: isValidPayment}}
     }catch(e){
       console.log(e)
-      return { statusCode: 200, body:{message: 'test ok'}}
+      return { statusCode: 404, body:{message: 'Payment not found'}}
     }
     
   }
