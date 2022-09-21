604f4cc97bdabfa53693178719d8781c0327b2cd
JoãoFerreiraNewWay <joao.ferreira@gruponewway.com.br>
2022-09-21 15:08:45 -0300

    refactor: add ajvValidator to getPaymentInfoController

--- src/factory/makeGetPaymentInfoByPaymentId.ts
+++ src/factory/makeGetPaymentInfoByPaymentId.ts
@@ -1,3 +1,4 @@
+import AjvValidatorAdapter from "../ adapter/ajvValidatorAdapter";
 import GetPaymentInfoByPaymentId from "../controller/paymentInfo/getPaymentInfoByPaymentId";
 import GetAllPaymentsController from "../controller/payments/getAllPaymentsController";
 import MongoPaymentsInfoRepository from "../repository/mongoPaymentInfoRepository";
@@ -9,6 +10,21 @@ export const makeGetPaymentInfoByPaymentId = () : GetPaymentInfoByPaymentId => {
   const paymentRepository = new MongoPaymentsRepository()
   const paymentService = new PaymentsService(paymentRepository)
   const paymentInfoRepository = new MongoPaymentsInfoRepository()
-  const paymentInfoService = new PaymentsInfoService(paymentInfoRepository, paymentRepository)
+  
+  const ajvValidator = new AjvValidatorAdapter({
+    type: "object",
+    properties: {
+        paymentId: {type: "string"},
+        title: {type: "string"},
+        icon: {type: "string"},
+        treasurer: {type: "string"},
+        formatedPhone: {type: "string"},
+        phone: {type: "string"},
+        pix: { type: "object" },
+        ted: { type: "object" }
+    },
+    required: ["paymentId", "title", "icon", "treasurer", "formatedPhone", "phone"],
+  })
+  const paymentInfoService = new PaymentsInfoService(paymentInfoRepository, paymentService, ajvValidator)
   return new GetPaymentInfoByPaymentId(paymentInfoService, paymentService)
 }
